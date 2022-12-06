08c8f0397e64f7fbba75f4a1511cda2d8a53dd6b
Norbert Herczeg <noherczeg@gmail.com>
2022-12-06 02:02:17 +0100

    fix jdk script

--- get-jdk.sh
+++ get-jdk.sh
@@ -2,12 +2,12 @@
 
 ZULU_VERSION=11.60.19
 
-rm zulu11.60.19-ca-jdk11.0.17-linux_x64.tar.gz
+rm zulu${ZULU_VERSION}-ca-jdk11.0.17-linux_x64.tar.gz
 
 wget https://cdn.azul.com/zulu/bin/zulu${ZULU_VERSION}-ca-jdk11.0.17-linux_x64.tar.gz
 
 tar -xvzf zulu${ZULU_VERSION}-ca-jdk11.0.17-linux_x64.tar.gz
 
-rm zulu11.60.19-ca-jdk11.0.17-linux_x64.tar.gz
+rm zulu${ZULU_VERSION}-ca-jdk11.0.17-linux_x64.tar.gz
 
 mv zulu${ZULU_VERSION}-ca-jdk11.0.17-linux_x64 jdk11
